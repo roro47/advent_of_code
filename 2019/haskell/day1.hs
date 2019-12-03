3f5387f52dd210a89ef1e195003c1d9b2858231e
roro47 <ryro@ryrodeMacBook-Pro.local>
2019-12-03 22:48:42 +0000

    complete day1: part1 and part2

--- 2019/haskell/day1.hs
+++ 2019/haskell/day1.hs
@@ -5,9 +5,19 @@ input_path = "../input/day1.txt"
 main :: IO ()
 main = do
     content <- readFile input_path
-    let input = map (read::String->Int) (lines content)
-        sumFuel = sum $ map getFuel input in
-        print sumFuel
+    input <- return $ map (read::String->Int) (lines content)
+    print $ "part1 " ++ show (sum $ map getFuel1 input)
+    print $ "part2 " ++ show (sum $ map getFuel2 input)
+        
 
-getFuel :: Int -> Int
-getFuel mass = mass `div` 3 - 2
+-- part1 fuel
+getFuel1 :: Int -> Int
+getFuel1 mass = mass `div` 3 - 2
+
+-- part2 fuel
+getFuel2 :: Int -> Int
+getFuel2 mass = getFuel' mass
+  where getFuel' mass'
+          | mass' <= 0 = 0
+          | mass == mass' = getFuel' (getFuel1 mass')
+          | otherwise = mass' + getFuel' (getFuel1 mass')
---
+++ 2019/haskell/day2.hs
@@ -0,0 +1,8 @@
+import System.IO
+
+input_path = "../input/day2.txt"
+
+main :: IO ()
+main = do
+    content <- readFile input_path
+    
